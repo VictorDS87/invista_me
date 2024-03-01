e933f70984f2bbb97e0a9ddb833e90070f5da1ff
Victor Donizete <victor@gw2d.com.br>
2024-03-01 15:38:41 -0300

    config de permissão host

--- invista_me/settings.py
+++ invista_me/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4gloyfxa6)c^5lwmkn86^z(imcur0t%o#nxx-tn%jo2m=pc2gs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
